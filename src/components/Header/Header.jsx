a27a132510d457ef82a1fc1e747d89477ffaf113
Gian2077 <gianrodrigues.2077@gmail.com>
2025-12-27 06:36:59 -0300

    feat: Add App Initial Structure

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import "./Header.css";
+export function Header() {
+  return (
+    <>
+      <header>
+        <h1>Tasks</h1>
+      </header>
+    </>
+  );
+}
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,8 @@
+import "./Main.css";
+export function Main() {
+  return (
+    <>
+      <main></main>
+    </>
+  );
+}
