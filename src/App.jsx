f91813df74fb906c866d7c34f21379e6f7050784
Gian2077 <gianrodrigues.2077@gmail.com>
2025-12-30 06:40:28 -0300

    feat: Import Tasks and Task Components to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import "./App.css";
 import { Header } from "./components/Header/Header.jsx";
 import { Main } from "./components/Main/Main.jsx";
+import { Tasks } from "./components/Tasks/Tasks.jsx";
+import { Task } from "./components/Task/Task.jsx";
 import { Footer } from "./components/Footer/Footer.jsx";
 
 function App() {
   return (
     <>
-      <Header></Header>
-      <Main></Main>
-      <Footer></Footer>
+      <Header />
+      <Main>
+        <Tasks>
+          <Task></Task>
+        </Tasks>
+      </Main>
+      <Footer />
     </>
   );
 }
