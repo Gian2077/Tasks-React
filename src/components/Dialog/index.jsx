5a1ef324b4d37656307d8347b672cfacfac9ef65
Gian2077 <gianrodrigues.2077@gmail.com>
2026-01-17 06:11:54 -0300

    feat: Add Heading

--- src/components/Dialog/index.jsx
+++ src/components/Dialog/index.jsx
@@ -1,6 +1,8 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, use } from "react";
 import styles from "./Dialog.module.css";
+import taskContext from "../TaskProvider/TaskContext.js";
 export function Dialog({ isOpen, onClose, children }) {
+  const { targetTask } = use(taskContext);
   const dialogRef = useRef(null);
   useEffect(() => {
     if (isOpen) {
@@ -19,7 +21,9 @@ export function Dialog({ isOpen, onClose, children }) {
     <>
       <dialog className={styles.dialog} ref={dialogRef}>
         <div className={styles.header}>
-          <h2 className={styles.heading}>Task</h2>
+          <h2 className={styles.heading}>
+            {targetTask ? "Edit Task" : "Add Task"}
+          </h2>
           <button className={styles.btn} onClick={onClose}>
             <i className="bi bi-x"></i>
           </button>
