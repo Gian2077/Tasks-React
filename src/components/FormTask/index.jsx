3116b176d7cef0db6c53bcacb6d30e1e90a9607c
Gian2077 <gianrodrigues.2077@gmail.com>
2026-01-19 05:25:11 -0300

    refactor(Form Task Component): Add Wrapper to Label/Input Pairs and Rename Labels to Better Describe What the Input Field Expects

--- src/components/FormTask/index.jsx
+++ src/components/FormTask/index.jsx
@@ -3,14 +3,16 @@ export function FormTask({ onSubmit, defaultValue }) {
   return (
     <>
       <form action={onSubmit} className={styles.form}>
-        <label htmlFor="title">Task</label>
-        <input
-          type="text"
-          id="title"
-          name="title"
-          required
-          defaultValue={defaultValue}
-        />
+        <div className={styles.wrapper}>
+          <label htmlFor="title">Title</label>
+          <input
+            type="text"
+            id="title"
+            name="title"
+            required
+            defaultValue={defaultValue}
+          />
+        </div>
         <button className={styles.btn}>Add Task</button>
       </form>
     </>
