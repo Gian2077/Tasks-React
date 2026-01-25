56aea8545277e156a8fbfa014dc6fd1025fb7ade
Gian2077 <gianrodrigues.2077@gmail.com>
2026-01-25 05:21:35 -0300

    feat: Add functionality to automatically reset a task status given a period of time has passed(day, week, month and year)

--- src/components/TaskProvider/TaskProvider.jsx
+++ src/components/TaskProvider/TaskProvider.jsx
@@ -2,10 +2,67 @@ import { useEffect, useState } from "react";
 import TaskContext from "./TaskContext.js";
 export function TaskProvider({ children }) {
   const key = "tasks";
+  const DAY = 24 * 60 * 60 * 1000;
+  const WEEK = DAY * 7;
+  const MONTH = DAY * 30;
+  const YEAR = DAY * 365;
   const savedTasks = localStorage.getItem(key);
   const [tasks, setTasks] = useState(savedTasks ? JSON.parse(savedTasks) : []);
   const [showDialog, setShowDialog] = useState(false);
   const [targetTask, setTargetTask] = useState();
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTasks((prev) => {
+        return prev.map((task) => {
+          if (!task.completed) return task;
+          const completedAt = new Date(task.dateCompleted).getTime();
+          const dateNow = Date.now();
+          switch (task.type) {
+            case "daily":
+              if (dateNow - completedAt >= DAY) {
+                return {
+                  ...task,
+                  completed: !task.completed,
+                  dateCompleted: null,
+                };
+              }
+              break;
+            case "weekly":
+              if (dateNow - completedAt >= WEEK) {
+                return {
+                  ...task,
+                  completed: !task.completed,
+                  dateCompleted: null,
+                };
+              }
+              break;
+            case "monthly":
+              if (dateNow - completedAt >= MONTH) {
+                return {
+                  ...task,
+                  completed: !task.completed,
+                  dateCompleted: null,
+                };
+              }
+              break;
+            case "yearly":
+              if (dateNow - completedAt >= YEAR) {
+                return {
+                  ...task,
+                  completed: !task.completed,
+                  dateCompleted: null,
+                };
+              }
+              break;
+            default:
+              return;
+          }
+          return task;
+        });
+      });
+    }, 60 * 1000);
+    return () => clearInterval(interval);
+  }, []);
   useEffect(() => {
     localStorage.setItem(key, JSON.stringify(tasks));
   }, [tasks]);
